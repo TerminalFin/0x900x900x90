e0bf55e5cb41e078eb99644cf4e42649da2fa120
Josh Levine <josh@cantreach.me>
2020-07-30 14:40:08 -0500

    Finished batch mode and fixed global vars

--- PowerShell/ps-panw-api/ps-panw-api.ps1
+++ PowerShell/ps-panw-api/ps-panw-api.ps1
@@ -1,9 +1,10 @@
 param (
-    [String] $Hostname,
-    [String] $Command,
-    [String] $Batch,
-    [Boolean] $DisplayAll,
-    [Boolean] $PauseBetweenFW
+    [String] $Hostname="",
+    [String] $Command="",
+    [String] $Batch="",
+    [String] $OutFile="",
+    [Boolean] $DisplayAll=$true,
+    [Boolean] $PauseBetweenFW=$true
 )
 
 # Ask whether or not to pause between outputs in batch mode
@@ -14,10 +15,10 @@ function Set-BatchPause() {
     } until ($PauseBetweenBatches -eq "Y" -or $PauseBetweenBatches -eq "N")
     
     if ($PauseBetweenBatches -eq "Y") {
-        Set-Variable "PauseBetweenFW" -Value $true -Scope global
+        $Global:Var_Pause_Between_FW = $true
     }
     else {
-        Set-Variable "PauseBetweenFW" -Value $false -Scope global
+        $Global:Var_Pause_Between_FW -eq $false
     }
 }
 
@@ -27,12 +28,12 @@ function Set-DisplayAll() {
         $display_all = read-host "(P)aginate or Display (A)ll results (p/a): "
         $display_all = $display_all.toupper()
     } until ($display_all -eq "A" -or $display_all -eq "P")
-    
+
     if ($display_all -eq "P") {
-        set-variable -Name "DisplayAll" -Value $false -Scope global
+        $Global:Var_Display_All = $false
     }
     else {
-        set-variable -Name "DisplayAll" -Value $true -Scope global
+        $Global:Var_Display_All = $true
     }
 }
 
@@ -42,15 +43,15 @@ function Invoke-Pause() {
     $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 }
 
-function Get-SplitCommand($Command) {
+function Get-SplitCommand($var_Command) {
     $split_command = ""
     # Split the command string into individual tags
-    foreach($item in $command.Split(" ")) {
+    foreach($item in $var_Command.Split(" ")) {
         $split_command += "<" + $item + ">"
     }
     
     # Reverse the order of the command string to terminate the individual command tags
-    $reverse_command = @($command.split(" "))
+    $reverse_command = @($var_Command.split(" "))
     [array]::reverse($reverse_command)
     foreach($item in $reverse_command) {
         $split_command += "</" + $item + ">"
@@ -77,45 +78,54 @@ function Show-XMLResponse($response) {
     }
     
     # Is this a batch operation? If not, then pause after output
-    if ($PauseBetweenFW) {
+    if ($Global:Var_Pause_Between_FW) {
         Invoke-Pause
     }
 }
 
-function Invoke-CLICommand($command, $fw_list) {
+function Invoke-CLICommand($var_Command, $fw_list) {
 
     # Command to execute not passed via CLI
-    if ($command -eq "") {
-        $command = Read-Host "Enter CLI command to execute: "
+    if ($var_Command -eq "") {
+        $var_Command = Read-Host "Enter CLI command to execute: "
+        Set-DisplayAll
     }
-    if ("log" -in $command) {
+    if ("log" -in $var_Command) {
         $action = "log"
     }
     else {
         $action = "op"
     }
     
-    if ($Batch -ne ""){
+    if ($var_Batch -ne ""){
         foreach ($item in $fw_list) {
 
             # Get FW specific API key
             $api_key = Get-APIKey $XML_Credential $item
 
             # Generate FW_URL for $item
-            $fw_url = "https://$item/api/?type=$action&cmd=$(Get-SplitCommand($command))&key=$api_key"
+            $fw_url = "https://$item/api/?type=$action&cmd=$(Get-SplitCommand($var_Command))&key=$api_key"
 
-            $XML_Response = Invoke-WebRequest -SkipCertificateCheck -Uri $fw_url
-
+            if ($OS -eq "linux") {
+                $XML_Response = Invoke-WebRequest -SkipCertificateCheck -Uri $fw_url
+            }
+            elseif ($OS -eq "windows") {
+                $XML_Response = Invoke-WebRequest -Uri $fw_url
+            }
             Show-XMLResponse $XML_Response
         }
     }
-    elseif ($Batch -eq "") {
+    elseif ($var_Batch -eq "") {
 
         # Generate $fw_url
-        $fw_url = "https://$hostname/api/?type=$action&cmd=$(Get-SplitCommand($command))&key=$api_key"
+        $fw_url = "https://$var_hostname/api/?type=$action&cmd=$(Get-SplitCommand($var_Command))&key=$api_key"
 
-        $XML_Response = Invoke-WebRequest -SkipCertificateCheck -Uri $fw_url
-    
+        if ($OS -eq "linux") {
+            $XML_Response = Invoke-WebRequest -SkipCertificateCheck -Uri $fw_url
+        }
+        elseif ($OS -eq "windows") {
+            $XML_Response = Invoke-WebRequest -Uri $fw_url
+        }
         Show-XMLResponse $XML_Response
         Invoke-Pause
     }
@@ -159,7 +169,7 @@ function Get-XMLTree($xml) {
         
         #Output "path = text-value"
         "$path = $(($node.'#text').Trim())"
-        if (($count % 10 -eq 0) -and (-not $DisplayAll)) {
+        if (($count % 10 -eq 0) -and (-not $var_Display_All)) {
             Invoke-Pause
             $count = 0
         }
@@ -174,31 +184,69 @@ function Get-APIKey($cred, $hostn) {
     $URL = "https://$hostn/api/?type=keygen&user=$User&password=$Pass"
     $User = ""
     $Pass = ""
-    [XML]$api_key = Invoke-WebRequest -SkipCertificateCheck -Uri $URL
+    if ($OS -eq "linux") {
+        [XML]$api_key = Invoke-WebRequest -SkipCertificateCheck -Uri $URL
+    }
+    elseif ($OS -eq "windows") {
+        [XML]$api_key = Invoke-WebRequest -Uri $URL
+    }
+    
     return $api_key.response.result.key
 }
 
 ### MAIN ROUTINE ###
 
+# Convert script parameters to global variables
+
+$Global:Var_Display_All = $DisplayAll
+$Global:Var_Pause_Between_FW = $PauseBetweenFW
+$Global:Var_Command = $Command
+$Global:Var_Hostname = $Hostname
+$Global:Var_Batch = $Batch
+
+# First things first, let's check the OS version to determine how self-signed certs will be handled
+
+if (Test-Path env:OS) {
+    $OS = "windows"
+
+    # Configure Powershell to ignore self-signed certificates
+    add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+}
+    elseif (Test-Path env:SHELL) {
+    $OS = "linux"
+}
+else {
+    $OS = "linux"
+}
+
 <# --- DEBUG CODE TO VERIFY CLI ARGUMENTS
-write-host "DisplayAll = $DisplayAll"
-write-host "PauseBetweenFW = $PauseBetweenFW"
-write-host "Command = $Command"
-write-host "Batch = $Batch"
+write-host "DisplayAll = $var_Display_All"
+write-host "PauseBetweenFW = $var_Pause_Between_FW"
+write-host "Command = $var_Command"
+write-host "Batch = $var_Batch"
 #>
 
-Set-Variable -name "Hostname" -Scope global
-
-if ($Hostname -eq "" -and $Batch -eq ""){
-    $Hostname = Read-host "Enter firewall IP address or FQDN: "
+if ($var_hostname -eq "" -and $var_Batch -eq ""){
+    $var_hostname = Read-host "Enter firewall IP address or FQDN: "
 }
 
 # Read XML API Administrator account info
 $XML_Credential = Get-Credential -Message "Enter username and password for administrator with XML API permissions: "
 
 # Only call Get-APIKey here if we are not operating in batch mode (single firewall mode)
-if ($Batch -eq "") {
-    try {$api_key = Get-APIKey $XML_Credential $hostname }
+if ($var_Batch -eq "") {
+    try {$api_key = Get-APIKey $XML_Credential $var_hostname }
     catch { 
         "Invalid credential entered..."
         break
@@ -207,45 +255,48 @@ if ($Batch -eq "") {
 
 #### INCOMPLETE ####
 # Parse script arguments
-if ($Batch -ne "") {
-    if (-not (Test-Path $Batch)) {
+if ($var_Batch -ne "") {
+    if (-not (Test-Path $var_Batch)) {
         Write-Host "Batch file not found"
         break
     }
 }
 
 # Running in batch mode, so process batch mode specific inputs
-if ($Batch -ne "") {
-    if ($DisplayAll -ne $true -and $DisplayAll -ne $false) {
+if ($var_Batch -ne "") {
+    if ($var_Display_All -ne $true -and $var_Display_All -ne $false) {
         Set-DisplayAll
     }
-    if ($PauseBetweenFW -ne $true -and $PauseBetweenFW -ne $false) {
+    if ($var_Pause_Between_FW -ne $true -and $var_Pause_Between_FW -ne $false) {
         Set-BatchPause
     }
 }
 
 <# Not batch mode with no command argument specified
-if ($Command -eq "" -and $Batch -eq "") {
+if ($var_Command -eq "" -and $var_Batch -eq "") {
     Invoke-CLICommand
     break
 }#>
 
 # Not batch mode but command argument specified
-if ($Command -ne "" -and $Batch -eq ""){
-    Invoke-CLICommand $Command
+if ($var_Command -ne "" -and $var_Batch -eq ""){
+    Write-Host "--- Single Command Processing Mode ---"
+    Invoke-CLICommand $var_Command
 }
 
 # Batch mode with no command argument specified
-elseif ($Command -eq "" -and $Batch -ne "") {
-    $fw_list = Get-Content -Path $Batch
+elseif ($var_Command -eq "" -and $var_Batch -ne "") {
+    Write-Host "--- Batch Processing Mode ---"
+    $fw_list = Get-Content -Path $var_Batch
     Invoke-CLICommand "" $fw_list
     break
 }
 
 # Batch mode with command argument specified
-elseif ($Command -ne "" -and $Batch -ne "") {
-    $fw_list = Get-Content -Path $Batch
-    Invoke-CLICommand $Command $fw_list
+elseif ($var_Command -ne "" -and $var_Batch -ne "") {
+    Write-Host "--- Batch Processing Mode ---"
+    $fw_list = Get-Content -Path $var_Batch
+    Invoke-CLICommand $var_Command $fw_list
     break
 }
 
@@ -268,11 +319,13 @@ do {
     write-host "0. Exit Script"
     $selection = Read-Host "Enter your selection: "
 
+    $selection = $selection.Trim()
+
     switch($selection) {
         "1" {
             write-host "Generating API Key"
             $user_credential = Get-Credential -Message "Enter username and password to get API credential for"
-            $key = Get-APIKey $user_credential $hostname
+            $key = Get-APIKey $user_credential $var_hostname
             write-host $key
             Invoke-Pause
         }
@@ -299,12 +352,22 @@ do {
         "7" {
             write-host "`nGenerating a TSF file requires superuser permissions"
             $admin_creds = Get-Credential -Message "Please enter details for superuser account: "
+            
             write-host "Generating superuser API Key"
-            $admin_key = Get-APIKey $admin_creds $hostname
-            $URL = "https://$hostname/api/?type=export&category=tech-support&key=$admin_key"
+            $admin_key = Get-APIKey $admin_creds $var_hostname
+            $URL = "https://$var_hostname/api/?type=export&category=tech-support&key=$admin_key"
+            
             write-host "Generating TSF - Please stand by"
-            [XML]$tsf_gen_results = Invoke-WebRequest -Uri $URL -SkipCertificateCheck
+            
+            if ($OS -eq "linux") {
+                [XML]$tsf_gen_results = Invoke-WebRequest -Uri $URL -SkipCertificateCheck
+            }
+            elseif ($OS -eq "windows") {
+                [XML]$tsf_gen_results = Invoke-WebRequest -Uri $URL
+            }
+            
             write-host $tsf_gen_results
+            
             Invoke-Pause
             if ($tsf_gen_results.response.status -eq "success") {
                 write-host "TSF generated successfully"
@@ -320,8 +383,7 @@ do {
             Invoke-Pause
         }
         "10" {
-            Set-DisplayAll
-            Invoke-CLICommand "" "" $hostname
+            Invoke-CLICommand "" "" $var_hostname
         }
     }
 } While ($selection -ne 0)
